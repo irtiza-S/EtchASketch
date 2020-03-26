dc99122eb33e9763b240dc499692db831267ece9
irtiza-S <irtizasafdar@hotmail.co.uk>
2020-03-26 18:01:49 +0000

    got changeBackground() function to work, div2 event now fires correctly

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 const container = document.querySelector('.container');
 const square = document.getElementsByClassName('square');
-let colors = ['1', '2', '3', '4', '5'];
-let div2;
+const header = document.querySelector('#header');
 
 
-function changeBackground(){
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-    if(randomColor === '1'){
-        div2.style.backgroundColor = 'red';
-        console.log('red');
-    } else if(randomColor === '2'){
-        div2.style.backgroundColor = 'blue';
-        console.log('blue');
-    } else if(randomColor === '3'){
-        div2.style.backgroundColor = 'yellow';
-        console.log('yellow');
-    } else if(randomColor === '4'){
-        div2.style.backgroundColor = 'orange';
-        console.log('orange');
-    } else{
-        div2.style.backgroundColor = 'green';
-        console.log('green');
-    }
+function changeBackground(e){
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    let div2 = e.target; //this will target the current element - which is div2
+    return div2.style.backgroundColor = randomColor;
 }
 
 //makes a 16 x 16 grid - default grid
@@ -32,13 +16,15 @@ for(let i = 0; i < 16; i++){
     container.appendChild(div);
     // div.addEventListener('mouseover', changeBackground);
     for (let j = 0; j < 16; j++){
-        div2 = document.createElement('div');
+        const div2 = document.createElement('div');
         div2.classList.add('square');
         div.style.display = 'flex';
         div.appendChild(div2);  
         
         div2.addEventListener('mouseover', changeBackground);
+        
     }
+    
 }
 
 
